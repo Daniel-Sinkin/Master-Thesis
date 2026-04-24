ec754efffd906e7290c949c9cdc3be90dd7a9c1e
Daniel-Sinkin <danielsinkin97@gmail.com>
2026-04-24 18:46:49 +0200

    Adjustments in building

--- tensor-network/src/app/main.cpp
+++ tensor-network/src/app/main.cpp
@@ -2,6 +2,7 @@
 #include "models/transverse_ising.hpp"
 #include "tensor/environment.hpp"
 #include "tensor/mps.hpp"
+#include "tensor/tensor.hpp"
 
 #include <format>
 
@@ -9,22 +10,30 @@ int main()
 {
     using namespace ds_tn;
 
-    const auto mpo = transverse_ising_mpo(4, 1.0, 1.0);
-    const auto num_sites = mpo.size();
-    const auto max_bond_dim = 5;
-    auto mps = random_mps(
-        num_sites,
-        RandomMPSConfig{
-            .physical_dim = 2,
-            .max_bond_dim = max_bond_dim,
-            .seed = 0zu,
-        }
-    );
-    mps.right_orthogonalize();
-    const auto envs = right_environments(mps, mpo);
+    const auto lhs = Tensor::random_normal({10, 20, 30, 40, 50, 60});
+    lhs.print_metadata();
 
-    for (auto site = 0zu; site < envs.size(); ++site)
+    std::println("Hello");
+
+    if constexpr (false)
     {
-        envs[site].print_metadata(std::format("right_env_{}", site));
+        const auto mpo = transverse_ising_mpo(4, 1.0, 1.0);
+        const auto num_sites = mpo.size();
+        const auto max_bond_dim = 5;
+        auto mps = random_mps(
+            num_sites,
+            RandomMPSConfig{
+                .physical_dim = 2,
+                .max_bond_dim = max_bond_dim,
+                .seed = 0zu,
+            }
+        );
+        mps.right_orthogonalize();
+        const auto envs = right_environments(mps, mpo);
+
+        for (auto site = 0zu; site < envs.size(); ++site)
+        {
+            envs[site].print_metadata(std::format("right_env_{}", site));
+        }
     }
 }
